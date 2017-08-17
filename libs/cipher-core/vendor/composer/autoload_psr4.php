cef576b8f83e526487fd757ac6b634abadfee6ae
Wolfgang <wolfgang@crypteron.com>
2017-08-16 23:29:27 -0700

    adding phpunit with composer and a config so it "just works"

--- libs/cipher-core/vendor/composer/autoload_psr4.php
+++ libs/cipher-core/vendor/composer/autoload_psr4.php
@@ -6,7 +6,11 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'phpDocumentor\\Reflection\\' => array($vendorDir . '/phpdocumentor/reflection-common/src', $vendorDir . '/phpdocumentor/type-resolver/src', $vendorDir . '/phpdocumentor/reflection-docblock/src'),
+    'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
     'Assert\\' => array($vendorDir . '/beberlei/assert/lib/Assert'),
     'AESGCM\\' => array($vendorDir . '/spomky-labs/php-aes-gcm/src'),
 );
